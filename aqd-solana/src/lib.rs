1bb9c73e1289e2fdd6a5bef20543553ddbb098c5
Tarek <tareknaser360@gmail.com>
2023-10-16 20:38:02 +0300

    feat: put the crates in a separate folder
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- aqd-solana/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-mod call;
-mod deploy;
-
-pub use deploy::SolanaDeploy;
---
+++ crates/aqd-solana/src/lib.rs
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
+mod commands;
+
+pub use commands::deploy::SolanaDeploy;
